3097c64f2c4786987d92abec8b0070472ba58798
alex <alexward12011989@gmail.com>
2025-06-12 18:49:10 -0600

    add launchpad

---
+++ bin/blackice-launchpad.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+SESSION="BlackIce"
+
+#Start Tmux Session if it does not already exist
+if ! tmux has-session -t "$SESSION" 2>/dev/null; then
+    echo "[*] Launching BlackIce Ops Control Center..."
+
+    #create session and first window 
+    tmux new-session -d -s "$SESSION" -n "ops"
+
+    #Pane 1: MOTD + sys info
+    tmux send-keys -t "$SESSION":0.0 'clear && fastfetch && echo && checkupdates && echo && free -h && echo && df -h' C-m
+
+    # Split Pane 1 vertically -> Pane 2: btop
+    tmux split-window -h -t "$SESSION":0
+    tmux send-keys -t "$SESSION":0.1 'btop' C-m
+
+    #Split Pane 1 horizontally -> Pane 3: logs
+    tmux split-window -v -t "$SESSION":0.0
+    tmux send-keys -t "$SESSION":0.2 'journalctl -f -p warning' C-m
+
+    # Split Pane 2 horizontally -> Pane 4: playerctl
+    tmux split-window -v -t "$SESSION":0.1
+    tmux send-keys -t "$SESSION":0.3 'playerctl metadata --follow' C-m
+
+fi
+
+#Attach to Tmux session
+tmux attach -t "$SESSION"
